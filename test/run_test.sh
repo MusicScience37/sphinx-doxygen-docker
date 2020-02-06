37a551bb50ac8e472d795a2ced6fb5b5cc728e48
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2020-02-07 00:18:13 +0900

    test: change the directory in which documentations are built

--- test/run_test.sh
+++ test/run_test.sh
@@ -9,11 +9,11 @@ mkdir build
 doxygen
 
 sphinx-build -M html \
-    sphinx build/sphinx/html \
+    sphinx build/sphinx \
     -D breathe_projects.TestBreathe="$(dirname $0)/build/doxygen/xml" \
     -D "plantuml=java -jar ${PLANTUML_JAR_PATH}"
 
 sphinx-build -M latexpdf \
-    sphinx build/sphinx/latex \
+    sphinx build/sphinx \
     -D breathe_projects.TestBreathe="$(dirname $0)/build/doxygen/xml" \
     -D "plantuml=java -jar ${PLANTUML_JAR_PATH}"
