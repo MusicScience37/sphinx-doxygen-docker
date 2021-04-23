e5a532c3cc77358abc5bb301f685a9fe02cc6806
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-04-24 02:47:38 +0900

    doc: update version string

--- test/sphinx/conf.py
+++ test/sphinx/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, MusicScience37 (Kenta Kabashima)'
 author = 'Kenta Kabashima'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1'
+release = '3.2'
 
 
 # -- General configuration ---------------------------------------------------
