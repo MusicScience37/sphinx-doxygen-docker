a5f5ce3063724656bd348e052096425a1a0584cf
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2024-07-12 20:58:48 +0900

    Removed unused commands and packages.

--- test/run_test.sh
+++ test/run_test.sh
@@ -14,8 +14,3 @@ sphinx-build -M html \
     sphinx build/sphinx \
     -D breathe_projects.TestBreathe="$(dirname $0)/build/doxygen/xml" \
     -D "plantuml=java -jar ${PLANTUML_JAR_PATH}"
-
-sphinx-build -M latexpdf \
-    sphinx build/sphinx \
-    -D breathe_projects.TestBreathe="$(dirname $0)/build/doxygen/xml" \
-    -D "plantuml=java -jar ${PLANTUML_JAR_PATH}"
